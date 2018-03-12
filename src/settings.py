0f674107352411b2e945dc5fece819db3d10b955
e.pogrebnyak@gmail.com <e.pogrebnyak@gmail.com>
2018-03-12 12:24:06 +0300

    simplified downloading

---
+++ src/settings.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""Directory structure and other configuration."""
+
+import os
+import platform
+
+# archives to download from Rosstat
+URL = {2012: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2012/data-20161021t000000-structure-20121231t000000.rar',
+       2013: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2013/data-20161021t000000-structure-20131231t000000.rar',
+       2014: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2014/data-20161021t000000-structure-20141231t000000.rar',
+       2015: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2015/data-20161021t000000-structure-20151231t000000.rar',
+       2016: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2016/data-20171023t000000-structure-20161231t000000.rar'
+       }
+
+VALID_YEARS = list(URL.keys())
+
+
+class RemoteStorage:
+    targets = \
+    {2012: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2012/data-20161021t000000-structure-20121231t000000.rar',
+     2013: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2013/data-20161021t000000-structure-20131231t000000.rar',
+     2014: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2014/data-20161021t000000-structure-20141231t000000.rar',
+     2015: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2015/data-20161021t000000-structure-20151231t000000.rar',
+     2016: 'http://www.gks.ru/opendata/storage/7708234640-bdboo2016/data-20171023t000000-structure-20161231t000000.rar'
+}
+    
+    def __init__(self, year: int):
+        self.url = self.targets[year]
+        
+    @property    
+    def rar_file(self):
+        
+        
+
+
+# RAR executable
+IS_WINDOWS = (platform.system() == 'Windows')
+
+if IS_WINDOWS:
+    UNPACK_RAR_EXE = os.path.join('bin', 'unrar.exe')
+else:
+    UNPACK_RAR_EXE = 'unrar'
